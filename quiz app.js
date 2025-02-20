f4abf7aa1541496fa9ef1a0322d4974919edcc93
Fortune1960 <nelsonfortune0@gmail.com>
2025-02-20 15:12:22 +0100

    5th update

--- quiz app.js
+++ quiz app.js
@@ -97,7 +97,8 @@ function selectAnswer(e){
 
 function showScore(){
     resetState();
-    questionElement.innerHTML = 'You scored ${score} out of ${questions.length}!';
+    questionElement.innerHTML = `You scored ${score} out of ${questions.
+        length}!`;
     nextButton.innerHTML = "Play Again";
     nextButton.style.display = "block";
 }
